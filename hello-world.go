edfce7d909c244f3a0ef03d6204534733b1e156b
William Pepple <williampepple1@gmail.com>
2022-07-22 10:43:15 +0100

    how to build a simple program

--- hello-world.go
+++ hello-world.go
@@ -5,3 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Hello world")
 }
+
+// If the program is more than a one-shot experiment, it’s likely that you would want to compile
+// it once and save the compiled result for later use. That is done with go build:
+// $ go build helloworld.go
